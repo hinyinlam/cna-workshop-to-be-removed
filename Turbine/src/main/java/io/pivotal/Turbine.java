c3fab29395f7e1dfc850b02ec60669160a40c9dd
HinLam <hlam@pivotal.io>
2016-04-06 12:15:05 +0800

    Adding Turbine

---
+++ Turbine/src/main/java/io/pivotal/Turbine.java
@@ -0,0 +1,19 @@
+package io.pivotal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
+
+@SpringBootApplication
+@EnableTurbine
+@EnableDiscoveryClient
+public class Turbine {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(Turbine.class, args);
+    }
+
+
+}
